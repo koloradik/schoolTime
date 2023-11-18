78ca251589bd3c140654fbd8e09b48350259bdf1
T1myr <robobox2020@gmail.com>
2023-11-18 18:57:22 +0200

    2

--- src/components/header/Header/Account.tsx
+++ src/components/header/Header/Account.tsx
@@ -25,11 +25,24 @@ export default function Account() {
         // </div>
         <HoverCard.Root>
           <HoverCard.Trigger asChild>
-            <button>asdasdasdasd</button>
+            <div className="group">
+              <button className="group-data-[state=open]:invisible">
+                asdasdasdasd
+              </button>
+            </div>
           </HoverCard.Trigger>
           <HoverCard.Portal>
-            <HoverCard.Content className="hidden group-hover:flex w-60 h-96 hover:bg-white rounded-md duration-500 bg-white/5 mr-1 justify-center items-center">
-              <button className="flex items-center justify-center gap-2 bg-white font-semibold w-52 h-10 rounded-lg hover:bg-white/5 hover:border hover:border-white hover:text-white hover:bg-slate-700 duration-300">
+            <HoverCard.Content className="flex flex-col bg-yellow-200 px-6">
+              <button className="">
+                <p>Google</p>
+              </button>
+              <button className="">
+                <p>Google</p>
+              </button>
+              <button className="">
+                <p>Google</p>
+              </button>
+              <button className="">
                 <p>Google</p>
               </button>
             </HoverCard.Content>
