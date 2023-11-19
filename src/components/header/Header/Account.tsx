7cc51874488788f83d8c59df6ca8f6b367b73a2c
T1myr <robobox2020@gmail.com>
2023-11-19 19:50:26 +0200

    push

--- src/components/header/Header/Account.tsx
+++ src/components/header/Header/Account.tsx
@@ -1,6 +1,5 @@
 import { User2 } from "lucide-react";
 import { useSession } from "next-auth/react";
-import * as HoverCard from "@radix-ui/react-hover-card";
 
 export default function Account() {
   const session = useSession();
@@ -9,45 +8,23 @@ export default function Account() {
     <>
       {!session ? (
         <button
-          className="transition-all duration-200 text-white/95 border-white border w-9 h-9 rounded-full flex justify-center items-center hover:bg-purple-800 text-lg mr-6
+          className="transition-all text-white/95 border-white border w-9 h-9 rounded-full flex justify-center items-center hover:bg-purple-800 text-lg mr-6 duration-200
       "
         >
           I
         </button>
       ) : (
-        // <div className="group flex">
-        //   <button className="flex group-hover:hidden text-white/95 border-white border w-9 h-9 rounded-full justify-center duration-200 items-center text-lg mr-1">
-        //     <User2 />
-        //   </button>
-        //   <button className="flex text-white/95 border-white border w-9 h-9 rounded-full justify-center duration-200 items-center text-lg mr-1">
-        //   <User2 />
-        // </button>
-        // </div>
-        <HoverCard.Root>
-          <HoverCard.Trigger asChild>
-            <div className="group">
-              <button className="group-data-[state=open]:invisible">
-                asdasdasdasd
-              </button>
-            </div>
-          </HoverCard.Trigger>
-          <HoverCard.Portal>
-            <HoverCard.Content className="flex flex-col bg-yellow-200 px-6">
-              <button className="">
-                <p>Google</p>
-              </button>
-              <button className="">
-                <p>Google</p>
-              </button>
-              <button className="">
-                <p>Google</p>
-              </button>
-              <button className="">
-                <p>Google</p>
-              </button>
-            </HoverCard.Content>
-          </HoverCard.Portal>
-        </HoverCard.Root>
+        <div className="group flex">
+          <button className="flex group-hover:hidden text-white/95 border-white border w-9 h-9 rounded-full justify-center duration-200 items-center text-lg mr-1">
+            <User2 />
+          </button>
+
+          <div className="hidden group-hover:flex text-white/95 bg-white w-60 h-60 rounded-md justify-center text-lg mr-1 mt-52">
+            <p className="text-black font-semibold">Авторизація</p>
+            <User2 className="w-24 h-24 text-black border-[6px] border-black rounded-full" />
+            <button></button>
+          </div>
+        </div>
       )}
     </>
   );
